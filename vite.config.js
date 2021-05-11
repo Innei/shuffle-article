c52fda560546a470d8f86cec742a1110d98282ec
Innei <tukon479@gmail.com>
2021-05-11 20:36:20 +0800

    chore: config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite'
-
+const { resolve } = require('path')
 export default defineConfig({
   base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        playground: resolve(__dirname, 'playground.html'),
+      },
+    },
+  },
 })
